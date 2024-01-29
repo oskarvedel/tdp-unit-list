d391f5a4baf18e408e31300230eebab6ec237db7
oskarvedel <oskar.vedel@gmail.com>
2024-01-29 10:44:48 +0100

    move booking handler

--- tdp-unit-list-plugin.php
+++ tdp-unit-list-plugin.php
@@ -7,7 +7,6 @@
 
 require_once dirname(__FILE__) . '/unit-list.php';
 require_once dirname(__FILE__) . '/tdp-common-unit-list.php';
-require_once dirname(__FILE__) . '/handle-booking-form.php';
 
 function custom_date_picker_scripts()
 {
